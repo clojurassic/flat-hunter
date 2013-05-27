0d5970aac0a4300ae92e0951b6a7e6e197cc2b1c
Chrislain Razafimahefa <razafima@gmail.com>
2013-05-27 21:59:05 +0200

    Added Chrislain's solution for getting each appart row at Brolliet.

---
+++ src/flat_hunter/brolliet.clj
@@ -0,0 +1,16 @@
+(ns flat-hunter
+  (:require [net.cgrand.enlive-html :as html]))
+
+(def *base-url* "http://www.brolliet.ch/fr/locataires/louer-un-bien")
+
+(defn fetch-url [url]
+  (html/html-resource (java.net.URL. url)))
+
+(defn br-apparts []
+  (partition 6 (map html/text (html/select (fetch-url *base-url*) #{[:td.localite]
+                                                                    [:td.rue]
+                                                                    [:td.type]
+                                                                    [:td.pieces]
+                                                                    [:td.surface]
+                                                                    [:td.montant]
+                                                                    }))))
