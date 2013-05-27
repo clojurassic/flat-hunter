c7af52c0a4134d73b87bf4a87db5d3ad6a0b1b65
Olivier Lange <olange@petit-atelier.ch>
2013-05-27 20:14:34 +0200

    Ajouté ébauche du projet Leiningen

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to flat-hunter
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/flat_hunter/core_test.clj
@@ -0,0 +1,7 @@
+(ns flat-hunter.core-test
+  (:use clojure.test
+        flat-hunter.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
