00d6d9200b6ed909e89671bc50a31054760b7753
Olivier Lange <olange@petit-atelier.ch>
2013-05-27 22:16:06 +0200

    Unification nom méthode pour retrouver appartements (de 'br-apparts' en 'flats')

--- src/flat_hunter/ingest/brolliet.clj
+++ src/flat_hunter/ingest/brolliet.clj
@@ -1,12 +1,12 @@
 (ns flat-hunter.ingest.brolliet
   (:require [net.cgrand.enlive-html :as html]))
 
-(def *base-url* "http://www.brolliet.ch/fr/locataires/louer-un-bien")
+(def ^:dynamic *base-url* "http://www.brolliet.ch/fr/locataires/louer-un-bien")
 
 (defn fetch-url [url]
   (html/html-resource (java.net.URL. url)))
 
-(defn br-apparts []
+(defn flats []
   (partition 6 (map html/text (html/select (fetch-url *base-url*) #{[:td.localite]
                                                                     [:td.rue]
                                                                     [:td.type]
