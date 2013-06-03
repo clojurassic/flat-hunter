73e9ed53aa66a654466a2c5c71e122118466d3e0
Olivier Lange <olange@petit-atelier.ch>
2013-06-03 23:12:00 +0200

    Complété extraction multipages des détails des objets à louer de ImmoStreet

--- src/flat_hunter/ingest/is.clj
+++ src/flat_hunter/ingest/is.clj
@@ -3,31 +3,89 @@
   					[hickory.select :as s]))
 
 
-(def url "http://www.immostreet.ch/fr/AdZone/Lookup/d1a01cea-17f5-41b4-94cf-1040a857b48e/2/MonthlyRent_ASC?resultPerPage=50")
+(def immostreet-results-url-pattern
+	"http://www.immostreet.ch/fr/AdZone/Lookup/d1a01cea-17f5-41b4-94cf-1040a857b48e/%d/MonthlyRent_ASC?resultPerPage=%d")
 
-;; Parse an URL and transform it to a hickory structure
+(defn result-pages
+	"Returns the list of URIs to scrape"
+	[num-pages results-per-page]
+	(map #(format immostreet-results-url-pattern %1 results-per-page) (range 1 (inc num-pages)))
+)
+
 (defn create-htree [url]
+	"Parse an URL and transform it to a hickory structure"
 	(-> url slurp h/parse h/as-hickory))
 
-;; Create a list of links to all flats classified ads
-(defn link-list []
-	;;FIXME: y-a pas plus simple ?
-	(map :href (map :attrs (s/select (s/descendant 
-																									(s/class "classified-content") 
-																									(s/tag :h3) 
-																									(s/and 	(s/tag :a)
-																													(s/attr :href))) 
-																		(create-htree url)))))
+(defn- links-to-flats* [results-htree]
+  "Create a sequence of links to all flats classified ads; the links are absolute, yet internal to the website"
+	(map :href
+		(map :attrs
+			(s/select
+				(s/descendant 
+					(s/class "classified-content") 
+					(s/tag :h3) 
+					(s/and (s/tag :a) (s/attr :href)))
+					results-htree))))
 
-(def results (link-list))
+(defn links-to-flats [one-result-page-url]
+	"Returns a sequence of all URIs to the detail pages describing the flats"
+	(let [doc   (create-htree one-result-page-url)
+		    links (links-to-flats* doc)
+	]
+		(map #(str "http://www.immostreet.ch" %) links)
+	))
 
-(def is-results (map #(str "http://www.immostreet.ch" %) results))
+(defn all-links-to-flats []
+	(flatten (map links-to-flats (result-pages 21 50))))
 
+(defn make-field
+	[flat-elt]
+	{ (-> flat-elt :attrs :id keyword)
+		(-> (s/select (s/child (s/tag :dl) (s/tag :dd)) flat-elt) first :content first) }
+)
 
-;; read each flat classified ad
-(defn read-articles []
-	(map create-htree is-results))
+(defn fetch-flat-details
+	"Returns a map with the details of a flat from an URI of its detail page"
+	[flat-page-uri]
+	(let [
+		doc (create-htree flat-page-uri)
+		flat-elements (s/select
+			(s/child
+			  (s/id "Detail-MainFeatures-panel")
+			  (s/tag :div)
+			  (s/and
+			  	(s/tag :dl)
+			  	(s/or
+			  		(s/id "AdMaterialImmoPropertySubTypeId")
+			  		(s/id "Address1")
+			  		(s/id "LocationID")
+			  		(s/id "RentalPriceWithoutExtraCosts")
+			  		(s/id "RentExtraCosts")
+			  		(s/id "LivingArea")
+			  		(s/id "NumberOfRooms")
+			  	))) doc)
+		  fields (reduce conj {} (map make-field flat-elements))
+		]
+		(zipmap [:description :adresse :lieu :loyer :charges :surface :nb-pieces]
+			[
+				(:AdMaterialImmoPropertySubTypeId fields)
+				(:Address1 fields)
+				(:LocationID fields)
+				(:RentalPriceWithoutExtraCosts fields)
+				(:RentExtraCosts fields)
+				(:LivingArea fields)
+				(:NumberOfRooms fields)
+			])
+		)
+)
 
+(defn flats
+	"Return all flats details"
+	[]
+  (time
+  	(doseq [ flat-page (all-links-to-flats) ]
+	  	(print (fetch-flat-details)) (flush)
+  )))
 
 ;; TODO: Read the details of each ad contained in 'is-results' and create a vector of result maps
 
